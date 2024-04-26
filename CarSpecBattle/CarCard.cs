371903c49ba8ad4e0aa8847d618d1a24ae96b77e
DKVERZIM <verena.zimmermann@LEGO.com>
2024-04-26 10:49:06 +0200

    recommitting class.

---
+++ CarSpecBattle/CarCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSpecBattle
+{
+    public class CarCard
+    {
+        public int CardID { get; set; }
+        public string Title { get; set; }
+        public int TopSpeed { get; set; }
+        public int Weight { get; set; }
+        public int HorsePower { get; set; }
+        public float Acceleration { get; set; }
+        public string EngineSize { get; set; }
+        /*Check with Kent*/
+        public string ImageLink { get; set; }
+        public int Tork { get; set; }
+    }
+}
